c1dde67a098880090b3dbdab27bbc2c1447b0ab3
marioskan <marios1997@yahoo.gr>
2020-04-05 04:45:30 +0300

    BB

--- android/app/src/main/java/com/example/ikesios/DiseaseStateme.java
+++ android/app/src/main/java/com/example/ikesios/DiseaseStateme.java
@@ -2,14 +2,20 @@ package com.example.ikesios;
 
 import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Calendar;
 
 public class DiseaseStateme extends AppCompatActivity {
     private RadioGroup radioGroup1;
@@ -18,6 +24,10 @@ public class DiseaseStateme extends AppCompatActivity {
     EditText date;
     DatePickerDialog datePickerDialog;
     TextView test;
+    TextView date1;
+    TextView date2;
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
+    private DatePickerDialog.OnDateSetListener mDateSetListener2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +36,84 @@ public class DiseaseStateme extends AppCompatActivity {
         test=(TextView)findViewById(R.id.test);
 
         test.setVisibility(View.INVISIBLE);
+        date1=(TextView)findViewById(R.id.date);
+        date2=(TextView)findViewById(R.id.date2);
+
+        Calendar cal= Calendar.getInstance();
+        int year=cal.get(Calendar.YEAR);
+        int month=cal.get(Calendar.MONTH);
+        int day=cal.get(Calendar.DAY_OF_MONTH);
+
+        month=month+1;
+        String yea=Integer.toString(year);
+        String mont=Integer.toString(month);
+        String da=Integer.toString(day);
+
+        date1.setText(da+" / "+mont+" / "+yea);
+        date2.setText(da+" / "+mont+" / "+yea);
+        date1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar cal= Calendar.getInstance();
+                int year=cal.get(Calendar.YEAR);
+                int month=cal.get(Calendar.MONTH);
+                int day=cal.get(Calendar.DAY_OF_MONTH);
+                DatePickerDialog dialog = new DatePickerDialog(
+                        DiseaseStateme.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        mDateSetListener,
+                        year,month,day);
+
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+
+        date2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar cal= Calendar.getInstance();
+                int year=cal.get(Calendar.YEAR);
+                int month=cal.get(Calendar.MONTH);
+                int day=cal.get(Calendar.DAY_OF_MONTH);
+                DatePickerDialog dialog = new DatePickerDialog(
+                        DiseaseStateme.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        mDateSetListener2,
+                        year,month,day);
+
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+        mDateSetListener2 = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int day) {
+                month=month+1;
+
+
+                String date= day + " / " +month+" / "+year;
+                date2.setText(date);
+            }
+        };
+
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int day) {
+                month=month+1;
+
+
+                String date= day + " / " +month+" / "+year;
+                date1.setText(date);
+            }
+        };
+
+
+
+
+
+
+
 
         submit=(Button)findViewById(R.id.submit_asthenia);
 
